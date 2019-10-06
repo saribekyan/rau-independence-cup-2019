852f79966f3f784cc02cd57f48a148fbae292dc3
Hayk Saribekyan <hayk.saribekyan@gmail.com>
2019-10-06 20:27:51 +0100

    reshuffle tests of A - first 10 tests are small

--- problems/A-bitonic/testgen.py
+++ problems/A-bitonic/testgen.py
@@ -7,7 +7,7 @@ import random
 
 script_path = os.path.realpath(__file__)
 dir_path    = os.path.dirname(script_path)
-test_dir    = os.path.join(dir_path, 'tests')
+test_dir    = os.path.join(dir_path, '../../tests/A')
 
 if not os.path.exists(test_dir):
     os.mkdir(test_dir)
@@ -35,25 +35,22 @@ print_test([1, 2, 3, 4, 2, 1])  # sample test 2
 
 print_test([1])
 
-# constant sequence
-print_test([10] * 100)
-print_test([MAX_H] * MAX_N)
+print_test([10] * 100) # constant sequence
+print_test(list(range(1, 101))) # increasing
+print_test(list(range(100, 0, -1))) # decreasing
+print_test(list(range(10, 0, -1)) + list(range(1, 11))) # \/
+print_test(list(range(1, 11)) + list(range(10, 0, -1))) # /\
 
-# increasing
-print_test(list(range(1, 101)))
-print_test(list(range(1, MAX_N + 1)))
+while curr_test <= N_TESTS / 2:
+    n = random.randint(1, 20000)
+    heights = [ random.randint(1, 20000) for i in range(n) ]
+    print_test(heights)
 
-# decreasing
-print_test(list(range(100, 0, -1)))
-print_test(list(range(MAX_N, 0, -1)))
-
-# \/
-print_test(list(range(10, 0, -1)) + list(range(1, 11)))
-print_test(list(range(MAX_N / 2, 0, -1)) + list(range(1, MAX_N / 2 + 1)))
-
-# /\
-print_test(list(range(1, 11)) + list(range(10, 0, -1)))
-print_test(list(range(1, MAX_N / 2 + 1)) + list(range(MAX_N / 2 + 100, 100, -1)))
+print_test([MAX_H] * MAX_N) # constant sequence
+print_test(list(range(1, MAX_N + 1))) # increasing
+print_test(list(range(MAX_N, 0, -1))) # decreasing
+print_test(list(range(MAX_N / 2, 0, -1)) + list(range(1, MAX_N / 2 + 1))) # \/
+print_test(list(range(1, MAX_N / 2 + 1)) + list(range(MAX_N / 2 + 100, 100, -1))) # /\
 
 while curr_test <= N_TESTS:
     n = random.randint(1, MAX_N)
