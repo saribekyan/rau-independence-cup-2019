4e57c786c4ac745d18056309763017c0aaa84b3f
Eduard Piliposyan <eduard.piliposyan@gmail.com>
2019-10-19 17:58:15 +0400

    Changed solution for M

--- problems/M-Messages/sol.cpp
+++ problems/M-Messages/sol.cpp
@@ -1,46 +1,69 @@
-#include <stdio.h>
 #include <iostream>
+#include <string>
+
 using namespace std;
-#define REP(i,n) for(int i=0;i<n;i++)
 
-struct node {
-    int part, end;
-    node *l, *r;
-} start;
-
-int main() {
-    int M, N; scanf("%d %d", &M, &N);
-    REP(i, M) {
-        int b; scanf("%d", &b);
-        node *at = &start;
-        REP(i, b) {
-            int bit; scanf("%d", &bit);
-            if (bit == 0) {
-                if (at->l == NULL) at->l = new node();
-                at = at->l;
-            }
-            else {
-                if (at->r == NULL) at->r = new node();
-                at = at->r;
-            }
-            at->part++;
+struct node
+{
+    int val;
+    int end, p;
+    node *n[2];
+} *head, *c;
+int main()
+{
+    int ans;
+    int n, m, p, t, i, j;
+    cin >> n >> m;
+    head = new node;
+    head->val = 0;
+    head->end = head->p = 0;
+    head->n[0] = head->n[1] = NULL;
+    for (i = 0; i < n; i++)
+    {
+        string st;
+        cin >> st;
+        c = head;
+        p = st.size();
+        for (j = 0; j < st.size(); j++)
+        {
+            t = (st[j] == '0' ? 0 : 1);
+            if (c->n[t] == NULL)
+                {
+                    c->n[t] = new node;
+                    c->n[t]->val = t;
+                    c->n[t]->n[0] = c->n[t]->n[1] = NULL;
+                    c->n[t]->p = 1;
+                    if (j+1 == p)
+                        c->n[t]->end = 1;
+                    else
+                        c->n[t]->end = 0;
+                }
+                else
+                {
+                    c->n[t]->p++;
+                    if (j+1 == p)
+                        c->n[t]->end++;
+                }
+            c = c->n[t];
         }
-        at->end++;
     }
-
-    REP(i, N) {
-        int b; scanf("%d", &b);
-        int matches = 0;
-        node *at = &start;
-        REP(i, b) {
-            int bit; scanf("%d", &bit);
-            if (at == NULL) continue;
-            matches += at->end;
-            if (bit == 0) at = at->l;
-            else at = at->r;
+    for (i = 0; i < m; i++)
+    {
+        ans = 0;
+        string st;
+        cin >> st;
+        p = st.size();
+        c = head;
+        for (j = 0; j < st.size() && c; j++)
+        {
+            t = (st[j] == '0' ? 0 : 1);
+            if (j+1 != p && c->n[t] != NULL)
+                ans += c->n[t]->end;
+            if (j+1 == p && c->n[t] != NULL)
+                ans += c->n[t]->p;
+            c = c->n[t];
         }
-        if (at != NULL) matches += at->part;
-        printf("%d\n", matches);
+        cout << ans << endl;
     }
     return 0;
 }
