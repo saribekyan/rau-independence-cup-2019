c38895f9bb78eebc9449ab074fdf54584838fb0c
Hayk Saribekyan <hayk.saribekyan@gmail.com>
2019-10-20 06:32:34 +0100

    improved tests for C - interactive, they were terrible

--- problems/C-scavenger/testgen.py
+++ problems/C-scavenger/testgen.py
@@ -1,4 +1,4 @@
-N_TESTS = 25
+N_TESTS = 100
 
 NMIN = 100
 NMAX = 1000
@@ -10,7 +10,7 @@ import math
 
 script_path = os.path.realpath(__file__)
 dir_path    = os.path.dirname(script_path)
-test_dir    = os.path.join(dir_path, '../../tests/C')
+test_dir    = os.path.join(dir_path, '..//..//tests//C')
 
 if not os.path.exists(test_dir):
     os.mkdir(test_dir)
@@ -22,7 +22,7 @@ def print_test(Delta):
 
     in_fname = os.path.join(test_dir, '%03d' % curr_test)
     ans_fname = os.path.join(test_dir, '%03d.a' % curr_test)
-    print in_fname
+    print(in_fname)
 
     n = len(Delta)
     assert(NMIN <= n and n <= NMAX)
@@ -43,21 +43,29 @@ def print_test(Delta):
     curr_test += 1
 
 def rand_test(n):
-    deltaMax = XMAX / n
+    deltaMax = XMAX // n
     Delta = [ None ] * n
     for i in range(n):
         Delta[i] = random.randint(-deltaMax, deltaMax)
     return Delta
 
 def powertwo_plus_one(n):
-    deltaMax = XMAX / n
+    deltaMax = XMAX // n
     l = int(math.log(deltaMax, 2))
 
     Delta = [ None ] * n
-    sign = 1
     for i in range(n):
-        Delta[i] = sign * ((1 << random.randint(0, l)) + 1)
-        sign *= -1
+        Delta[i] = (1 << (2 * random.randint(0, l // 2) + 1)) + 1
+    return Delta
+
+def power_plus_one(p):
+    n = random.randint(NMIN, NMAX)
+    deltaMax = XMAX // n
+    l = int(math.log(deltaMax, p))
+
+    Delta = [ None ] * n
+    for i in range(n):
+        Delta[i] = (p ** (2 * random.randint(0, l // 2) + 1)) + 1
     return Delta
 
 def negate(Delta):
@@ -66,40 +74,74 @@ def negate(Delta):
 def randSign(Delta):
     return [ random.choice([-1, 1]) * x for x in Delta ]
 
+def powertwoDelta_plus_one(n):
+    deltaMax = XMAX // n
+    Delta = [ ]
+
+    for i in range(n):
+        delta = 0
+        x = 1
+        prev = 0
+        while delta > -deltaMax:
+            prev = delta
+            delta += x
+            x *= -2
+            delta += x
+            x *= -2
+            if random.randint(0, 5) == 0:
+                break
+        Delta.append(prev - 1)
+
+    return Delta
+
+def powerDelta_plus_one(p):
+    n = random.randint(NMIN, NMAX)
+    deltaMax = XMAX // n
+    Delta = [ ]
+
+    for i in range(n):
+        delta = 0
+        x = 1
+        prev = 0
+        while delta > -deltaMax:
+            prev = delta
+            delta += x
+            x *= -p
+            delta += x
+            x *= -p
+            if random.randint(0, 5) == 0:
+                break
+        Delta.append(prev - 1)
+
+    return Delta
+
+
+def alternate(Delta, sign):
+    for i in range(len(Delta)):
+        Delta[i] = sign * Delta[i]
+        sign *= -1
+    return Delta
+
 print_test([ 1 ] * NMIN)
 print_test([ -1 ] * NMIN)
 
-print_test(randSign([ XMAX / NMAX ] * NMAX))
+print_test(randSign([ XMAX // NMAX ] * NMAX))
 
-print_test(rand_test(NMAX))
 print_test(rand_test(NMIN))
 
-print_test(powertwo_plus_one(NMIN))
-print_test(powertwo_plus_one(NMAX))
+for it in range(2):
+    for gen in [power_plus_one, powerDelta_plus_one]:
+        for p in range(2, 6):
+            for flip in [lambda x : alternate(x, -1), lambda x : alternate(x, 1), negate, (lambda x : x)]:
+                print_test(flip(gen(p)))
 
-print_test(negate(powertwo_plus_one(NMIN)))
-print_test(negate(powertwo_plus_one(NMAX)))
+n = random.randint(NMAX, NMAX)
+deltaMax = XMAX // n
+print_test([random.randint(-deltaMax, deltaMax)] * n)
+print_test([random.randint(-deltaMax, deltaMax)] * n)
 
-print_test(randSign(powertwo_plus_one(NMIN)))
-print_test(randSign(powertwo_plus_one(NMAX)))
-
-while curr_test <= N_TESTS:
-    n = random.randint(NMIN, NMAX)
-
-    ch = curr_test % 6
-
-    deltaMax = XMAX / n
-
-    if ch == 0:
-        print_test([random.randint(-deltaMax, deltaMax)] * n)
-    elif ch == 1:
-        print_test(rand_test(n))
-    elif ch == 2:
-        print_test(powertwo_plus_one(n))
-    elif ch == 3:
-        print_test(negate(powertwo_plus_one(n)))
-    elif ch == 4:
-        print_test(randSign(powertwo_plus_one(n)))
-    elif ch == 5:
-        print_test(randSign([ random.randint(-deltaMax, deltaMax) ] * n)) 
+n = random.randint(NMAX, NMAX)
+deltaMax = XMAX // n
+print_test([random.randint(-deltaMax, deltaMax)] * n)
+print_test([random.randint(-deltaMax, deltaMax)] * n)
 
